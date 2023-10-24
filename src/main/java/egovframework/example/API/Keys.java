340a3de20ef44c3c3b8e8100838fa440f2206a44
highlyko17 <highlyko17@gmail.com>
2023-10-24 19:32:15 +0900

    first commit

--- src/main/java/egovframework/example/API/Keys.java
+++ src/main/java/egovframework/example/API/Keys.java
@@ -2,7 +2,7 @@ package egovframework.example.API;
 
 public class Keys {
 	//add own key
-		public static String OPENAPI_KEY = "APIKEY"; 
+		public static String OPENAPI_KEY = "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"; 
 		//public static String OPENAPI_ORGKEY = "";
 		//public static String PINECONE_KEY = "";
 		
