596603d18ad4c18e3520a8db8f4a7d639b2f6321
LedF1oyd <defieroji@gmail.com>
2023-11-20 21:36:20 +0900

    update gitignore

--- src/main/java/egovframework/example/API/Keys.java
+++ src/main/java/egovframework/example/API/Keys.java
@@ -2,7 +2,7 @@ package egovframework.example.API;
 
 public class Keys {
 	//add own key
-		public static String OPENAPI_KEY = "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"; 
+		public static String OPENAPI_KEY = "아무거"; 
 		//public static String OPENAPI_ORGKEY = "";
 		//public static String PINECONE_KEY = "";
 		
