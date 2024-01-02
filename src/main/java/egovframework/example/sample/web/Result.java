18416370aca429176cf6e7bdabe4c10192480e8c
highlyko17 <highlyko17@gmail.com>
2024-01-02 16:10:22 +0900

    changed result.java

--- src/main/java/egovframework/example/sample/web/Result.java
+++ src/main/java/egovframework/example/sample/web/Result.java
@@ -3,10 +3,10 @@ package egovframework.example.sample.web;
 import java.util.Map;
 
 public class Result {
-	public Map<String, String> getResult(Map<String, String> response, long file_size, String summary_result, long executionTime) {
+	public Map<String, String> getResult(Map<String, String> response, long file_size, String result, long executionTime) {
 		response.put("isSuccess", "true");
 		response.put("finalFileSize", Long.toString(file_size) + " bytes");
-		response.put("summary_result", summary_result);
+		response.put("result", result);
 		response.put("executionTimeInMilli", Long.toString(executionTime));
 		
 		return response;
