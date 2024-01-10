7e7d0fc92d07936ec8e112d507db4548675fba6b
Here-Comes-The-Sun <139465547+Here-Comes-The-Sun@users.noreply.github.com>
2024-01-10 15:25:20 +0900

    Update Models.java

--- src/main/java/egovframework/example/API/Models.java
+++ src/main/java/egovframework/example/API/Models.java
@@ -1,7 +1,7 @@
 package egovframework.example.API;
 
 public class Models {
-	public static String WHISPER_MODEL = "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"; 
+	public static String WHISPER_MODEL = ""; 
 	public static String SUMMARY_MODEL = "";
 	//public static String OPENAPI_ORGKEY = "";
 	//public static String PINECONE_KEY = "";
